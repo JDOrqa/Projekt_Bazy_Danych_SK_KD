dd63f2767f216310a61b3ad7a6810727c65c882a
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2026-04-11 19:12:14 +0200

    Add user profile link to Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -31,6 +31,7 @@ function Navbar() {
             <li className="nav-item"><Link className="nav-link" to="/catches">Historia</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/upload-image">Dodaj zdjęcie</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/gatunki">Gatunki</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/userinfo">Mój profil</Link></li> 
             {user.roles?.includes('Admin') && (
               <li className="nav-item"><Link className="nav-link" to="/admin">Admin panel</Link></li>
             )}
